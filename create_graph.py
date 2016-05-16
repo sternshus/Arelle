61baa33ec5048609c9f4b71b7e27372a5f4739c6
redwardstern <ruth@sternshus.com>
2016-05-16 22:13:19 +0000

    updating arelle

---
+++ create_graph.py
@@ -0,0 +1,14 @@
+'''
+Created on Dec 21, 2015
+
+@author: redward
+'''
+import sys
+from arelle import CntlrCmdLine
+sys.argv = ['','-f', 'http://www.sec.gov/Archives/edgar/data/66740/000155837015002024/mmm-20150930.xml', 
+            '--disclosureSystem', 'efm-strict-all-years', '--store-to-XBRL-DB', 
+            'rdfTurtleFile,None,None,None,/home/redward/Downloads/turtle_3m.rdf,None,rdfDB']
+
+result = CntlrCmdLine.xbrlTurtleGraphModel('http://www.sec.gov/Archives/edgar/data/66740/000155837015002024/mmm-20150930.xml')
+
+x = 5
